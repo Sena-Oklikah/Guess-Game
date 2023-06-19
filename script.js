604fd1a1d9a9e33e69ade5b25d0b29ecfe1bb99c
Sena Oklikah <100801874+Sena-Oklikah@users.noreply.github.com>
2023-06-19 09:31:52 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+'use strict';
+// define secret number
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function() {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  // when there is no guess
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number';
+
+  // when guess is correct
+
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🍾🍾🍾 Correct number!';
+    document.querySelector('div.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+  // when guess is too high
+
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = ' 💥Game over!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+  // when guess is too low
+
+  } else if (guess < secretNumber) {
+    if (score > 11) {
+    document.querySelector('.message').textContent = '📉 Too Low';
+    score--;
+    document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥Game over!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
